2907b1ccd32fbb53e67125bae0d7a53421246def
D. Lisin <mrlisdim@gmail.com>
2019-09-06 19:38:20 +0300

    IMPR: Add requirements to a child class of Singleton

--- singleton/singleton.hpp
+++ singleton/singleton.hpp
@@ -10,8 +10,17 @@ namespace patterns {
 /**
  * @brief Inherit this class to make a child class a singleton
  *
- * If you make a private child constructor you should make this class a friend
- * of yours.
+ *
+ * ATTENTION!
+ * You MUST have only one private constructor without arguments!
+ * You should not have any public constructors to prevent your singleton-class
+ * instantiation not throw the `instance` interface.
+ *
+ * You should make this class a friend of yours to make it possible to
+ * instantiate your class inside the `instance` method.
+ *
+ * If you need to pass arguments to singleton constructor from outside make your
+ * own class. May be it is better to use a fabric pattern in this case.
  *
  * For example:
  *
@@ -42,7 +51,13 @@ struct Singleton : NonMovable<Singleton<T>>, NonCopyable<Singleton<T>>
     }
 
  protected:
-    Singleton() = default;
+    Singleton()
+    {
+        static_assert(
+          !std::is_default_constructible_v<T>,
+          "Your singleton class must not has any public constructors!");
+    }
+
     static T* instance_ptr;
 };
 
